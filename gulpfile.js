3214ba864c710a22d023b1408de6e7f3e8727545
Dragomir Urdov <dragomir.urdov@gmail.com>
2020-05-06 17:58:16 +0200

    initial project structure

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+
+const cssnano = require("gulp-cssnano");
+const autoprefixer = require("gulp-autoprefixer");
+
+function style() {
+    return gulp
+        .src("./src/sass/**/*.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(autoprefixer())
+        .pipe(cssnano())
+        .pipe(gulp.dest("./src/css"))
+        .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./src",
+        },
+    });
+    gulp.watch("./src/sass/**/*.scss", style);
+    gulp.watch("./src/*.html").on("change", browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
